519a2754f4a05047ddd26fe9f3dfd3eb0d043d89
Alberto G. Jácome <agjacome@gmail.com>
2015-08-06 20:22:47 +0200

    nix: enable broken, add ghc-mod cloned from github

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,16 +1,42 @@
-{
-  packageOverrides = pkgs : rec {
+{ pkgs }: {
 
-    haskellEnv = pkgs.haskellngPackages.ghcWithPackages (self : [
-      self.cabal2nix
-      self.cabal-install
-      self.hoogle
-      self.hlint
-      self.pandoc
-      self.mtl
-      self.text
-      self.QuickCheck
-    ]);
+  allowBroken = true;
+  allowUnfree = true;
+
+  packageOverrides = super: let self = super.pkgs; in
+    let inherit (self)   stdenv callPackage fetchFromGitHub;
+        inherit (stdenv) lib;
+        inherit (lib)    overrideDerivation concatMapStringsSep;
+    in rec {
+
+      haskellNGPackages = with self.haskell-ng.lib; self.haskell-ng.packages.ghc7101.override {
+        overrides = self: super: {
+          ghc-mod = overrideCabal super.ghc-mod (drv: {
+            broken = false;
+            src = pkgs.fetchFromGitHub {
+              owner  = "kazu-yamamoto";
+              repo   = "ghc-mod";
+              rev    = "f023d939e2edfc7e6874be76b5ad8f5de51e4124";
+              sha256 = "022ivz85xrbsxf3bls5cr8421a1va57fdcl7cgzskrkf3qgc5ji5";
+            };
+            buildDepends = drv.buildDepends ++ [ self.cabal-helper self.cereal ];
+            postInstall = "";
+          });
+        };
+      };
+
+      haskellEnv = haskellNGPackages.ghcWithPackages (self: [
+        self.cabal2nix
+        self.cabal-install
+        self.ghc-mod
+        self.hoogle
+        self.hlint
+        self.pandoc
+        self.mtl
+        self.text
+        self.QuickCheck
+      ]);
 
   };
+
 }
