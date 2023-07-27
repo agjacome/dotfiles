a5c80ea565c3603e0d64bd364ee130c943b57782
Alberto G. Jácome <contact@agjacome.dev>
2023-07-27 21:07:25 +0200

    fish: customize mcfly prompt

--- dot_config/private_fish/conf.d/mcfly.fish
+++ dot_config/private_fish/conf.d/mcfly.fish
@@ -5,5 +5,7 @@ if which mcfly > /dev/null;
     set -gx MCFLY_DISABLE_MENU true
     set -gx MCFLY_FUZZY 2
     set -gx MCFLY_KEY_SCHEME vim
+    set -gx MCFLY_PROMPT ''
+    set -gx MCFLY_RESULTS 30
     mcfly init fish | source;
 end
