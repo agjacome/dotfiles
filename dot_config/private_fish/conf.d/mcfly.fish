af89aba2007562d96264654ce4de0cf4378826d7
Alberto G. Jácome <contact@agjacome.dev>
2023-07-29 00:09:26 +0200

    fish: use type for checking existence of programs

--- dot_config/private_fish/conf.d/mcfly.fish
+++ dot_config/private_fish/conf.d/mcfly.fish
@@ -1,11 +1,11 @@
 status is-interactive || exit
 
-if which mcfly > /dev/null;
+if type -q mcfly
     set -gx MCFLY_DELETE_WITHOUT_CONFIRM true
     set -gx MCFLY_DISABLE_MENU true
     set -gx MCFLY_FUZZY 2
     set -gx MCFLY_KEY_SCHEME vim
     set -gx MCFLY_PROMPT ''
     set -gx MCFLY_RESULTS 30
-    mcfly init fish | source;
+    mcfly init fish | source
 end
