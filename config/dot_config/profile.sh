4e4e3481f0fad3ea9d3a40e88804edc8ad6ab77d
Alberto G. Jácome <contact@agjacome.dev>
2023-08-02 21:35:51 +0200

    xdg: make nix's vivaldi the default browser

--- config/dot_config/profile.sh
+++ config/dot_config/profile.sh
@@ -4,11 +4,6 @@ if [[ -f /etc/profile ]]; then
     source /etc/profile
 fi
 
-# FIXME: this is breaking stuff
-# if [[ -d $HOME/.nix-profile/share ]]; then
-#     export XDG_DATA_DIRS="$HOME/.nix-profile/share:$XDG_DATA_DIRS"
-# fi
-
 if [[ -z $DISPLAY && -n $XDG_VTNR && $XDG_VTNR -le 2 ]]; then
     if command -v tbsm >/dev/null 2>&1; then
         exec tbsm
