8343472f220114beaef3a40792dc4f354ca57a37
Alberto G. Jácome <contact@agjacome.dev>
2023-08-05 18:41:27 +0200

    fish: move config to multiple (sorted) conf.d files

---
+++ config/dot_config/fish/conf.d/01_setup_env.fish
@@ -0,0 +1,21 @@
+set -q __fish_setup_env; and return
+
+if test -f /etc/profile
+    fenv source /etc/profile
+end
+
+fish_add_path -p $HOME/.local/bin
+
+set -gx BROWSER vivaldi
+set -gx COLORTERM truecolor
+set -gx DIFFPROG nvim -d -c 'set diffopt+=vertical' -c 'set diffopt+=foldcolumn:0'
+set -gx EDITOR nvim
+set -gx GPG_TTY (tty)
+set -gx LANG en_GB.UTF-8
+set -gx LC_ALL en_GB.UTF-8
+set -gx LESS -R -X
+set -gx PAGER less
+set -gx QUOTING_STYLE literal
+set -fx LOCALE_ARCHIVE /usr/lib/locale/locale-archive
+
+set -gx __fish_setup_env 1
--- config/dot_config/fish/config.fish
+++ config/dot_config/fish/config.fish
@@ -1,24 +1,4 @@
-status is-interactive; or return
-
-set -q __fish_config_sourced; and return
-
-if test -f ~/.config/profile.sh
-    fenv source ~/.config/profile.sh
-end
-
-set -gx PATH $HOME/.local/bin $PATH
-
-set -gx BROWSER vivaldi
-set -gx COLORTERM truecolor
-set -gx DIFFPROG nvim -d -c 'set diffopt+=vertical' -c 'set diffopt+=foldcolumn:0'
-set -gx EDITOR nvim
-set -gx GPG_TTY (tty)
-set -gx LANG en_GB.UTF-8
-set -gx LC_ALL en_GB.UTF-8
-set -gx LESS -R -X
-set -gx PAGER less
-set -gx QUOTING_STYLE literal
-set -fx LOCALE_ARCHIVE /usr/lib/locale/locale-archive
+! status is-interactive; or set -q __fish_config_sourced; and return
 
 set -f theme_title_display_process yes
 set -f theme_title_display_path yes
@@ -27,17 +7,4 @@ set -f theme_title_use_abbreviated_path yes
 
 bind \ct $HOME/.local/bin/tmux-session
 
-if type -q direnv
-    direnv hook fish | source
-end
-
-if type -q fzf
-    set -gx FZF_DEFAULT_OPTS '--layout=reverse --cycle --ansi --height=100%'
-    set -gx FZF_TMUX 1
-end
-
-if type -q zoxide
-    zoxide init fish | source
-end
-
-set -gx __fish_config_sourced 0
+set -gx __fish_config_sourced 1
