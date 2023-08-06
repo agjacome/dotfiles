75a7193c537a5f63d5d4a2b40eb6a6b6d40f22d1
Alberto G. Jácome <contact@agjacome.dev>
2023-08-06 17:32:35 +0200

    fish: remove sourced flag in main config file

--- config/dot_config/fish/config.fish
+++ config/dot_config/fish/config.fish
@@ -6,13 +6,9 @@ if test -z $DISPLAY; and test -n $XDG_VTNR; and test $XDG_VTNR -le 2
     end
 end
 
-set -q __fish_config_sourced; and return
-
 set -f theme_title_display_process yes
 set -f theme_title_display_path yes
 set -f theme_title_display_user no
 set -f theme_title_use_abbreviated_path yes
 
 bind \ct $HOME/.local/bin/tmux-session
-
-set -gx __fish_config_sourced 1
