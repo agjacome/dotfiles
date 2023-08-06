0fabc462f3fa346ffe3c8ea012ce464f7afcf5a1
Alberto G. Jácome <contact@agjacome.dev>
2023-08-06 02:03:34 +0200

    fish: launch tbsm from fish (from previous profile.sh)

--- config/dot_config/fish/config.fish
+++ config/dot_config/fish/config.fish
@@ -1,4 +1,12 @@
-! status is-interactive; or set -q __fish_config_sourced; and return
+status is-interactive; or return
+
+if test -z $DISPLAY; and test -n $XDG_VTNR; and test $XDG_VTNR -le 2
+    if type -q tbsm
+        exec tbsm
+    end
+end
+
+set -q __fish_config_sourced; and return
 
 set -f theme_title_display_process yes
 set -f theme_title_display_path yes
