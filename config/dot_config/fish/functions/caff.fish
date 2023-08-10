5024be0004505ca96ecefb0605eb7ee32a4c5ebf
Alberto G. Jácome <alberto.jacome@frontify.com>
2023-08-10 06:34:15 +0200

    fish: caffeinate just first pgrep result on multiple matches

--- config/dot_config/fish/functions/caff.fish
+++ config/dot_config/fish/functions/caff.fish
@@ -1,6 +1,6 @@
 if type -q caffeinate; and type -q tmux
   function caff --wraps='caffeinate'
-    set -l pid (pgrep $argv)
+    set -l pid (pgrep $argv | head -1)
     tmux new-session -d -s caffeinate -n 'caffeinate' "caffeinate -diu -w$pid"
   end
 end
