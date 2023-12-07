6bcbe99631a99a56f21508a9073fa465461c7495
Alberto G. Jácome <alberto.jacome@frontify.com>
2023-12-07 15:52:44 +0100

    flake: bump stable to 23.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    stable.url = "github:nixos/nixpkgs/nixos-23.05";
+    stable.url = "github:nixos/nixpkgs/nixos-23.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
