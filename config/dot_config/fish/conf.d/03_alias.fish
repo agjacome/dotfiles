062928d71362aca0dfda5249dfe6abcab4072359
Alberto G. Jácome <alberto.jacome@frontify.com>
2024-05-10 10:15:44 +0200

    fish: always set tmux & docker aliases

--- config/dot_config/fish/conf.d/03_alias.fish
+++ config/dot_config/fish/conf.d/03_alias.fish
@@ -9,12 +9,6 @@ abbr mv    "mv -i"
 abbr rm    "rm -Ivf"
 abbr rr    "rm -Irvf"
 
-if type -q tmux
-    abbr ta "tmux attach -t"
-    abbr tl "tmux list-sessions"
-    abbr tn "tmux new -s (basename (pwd) | sed 's/[^a-zA-Z0-9]//g')"
-end
-
 if type -q nvim
     abbr v       "nvim"
     abbr vi      "nvim"
@@ -30,7 +24,7 @@ if type -q yay
     abbr qdt "yay -Qdtq"
     abbr ss  "yay -Ss"
     abbr syu "yay -Syu"
-else if test -f /usr/bin/pacman
+else if type -q pacman
     abbr i   "sudo pacman -S"
     abbr qdt "pacman -Qdtq"
     abbr ss  "pacman -Ss"
@@ -41,8 +35,10 @@ if test -d $HOME/.dotfiles
     abbr dotup "pushd $HOME/.dotfiles && git pull && home-manager switch --impure && chezmoi apply && popd"
 end
 
-if type -q docker
-    abbr dcu "docker compose up --build -d --remove-orphans --pull always --renew-anon-volumes --wait"
-    abbr dcp "docker compose ps -a --format='table {{.Name}}\t{{.Image}}\t{{.Status}}'"
-    abbr dcd "docker compose down"
-end
+abbr ta "tmux attach -t"
+abbr tl "tmux list-sessions"
+abbr tn "tmux new -s (basename (pwd) | sed 's/[^a-zA-Z0-9]//g')"
+
+abbr dcu "docker compose up --build -d --remove-orphans --pull always --renew-anon-volumes --wait"
+abbr dcp "docker compose ps -a --format='table {{.Name}}\t{{.Image}}\t{{.Status}}'"
+abbr dcd "docker compose down"
