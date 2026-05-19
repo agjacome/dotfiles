a8da8d1aa634d21e679540fe17fb92b4d6046152
Alberto G. Jácome <alberto.jacome@frontify.com>
2026-05-19 19:51:40 +0200

    feat(darwin): add helium, bump tableplus

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -13,22 +13,25 @@
   config = lib.mkIf config.homes.darwin.enable {
     targets.darwin.copyApps.enable = false;
     targets.darwin.linkApps.enable = true;
+
     home.packages = with pkgs; [
       alacritty
       coreutils-full
       gnused
+      helium
       nerd-fonts.monofur
       pinentry_mac
       raycast
       spotify
+      unixtools.watch
+
       (tableplus.overrideAttrs (oldAttrs: {
-        version = "662";
+        version = "704";
         src = fetchurl {
-          url = "https://files.tableplus.com/macos/662/TablePlus.dmg";
-          hash = "sha256-VR0sSTZfRjjv+p4DcYciKBJG5DHIwj4KLhTHPGRsSX0=";
+          url = "https://files.tableplus.com/macos/704/TablePlus.dmg";
+          hash = "sha256-u+vxksWZBUVtQRiR707S993rnuj3ofZaPvBglJlq1T8=";
         };
       }))
-      unixtools.watch
     ];
   };
 }
